af6c8243a8ac8b66a63ce15a47793db45848cfa8
Judiespa07 <judiespa07@gmail.com>
2024-07-09 21:45:14 -0500

    agregue pantallas

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto/screens/Home.dart';
+import 'package:librototal/screens/Home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home()
-    );
+    return MaterialApp(home: Home());
   }
 }
-
-
-
